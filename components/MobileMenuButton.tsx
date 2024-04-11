2b20d31da85e7aa13e8e32cb8d9e39cfe62cab9c
Norma Granados <paraisoocho@gmail.com>
2024-04-11 11:27:15 -0600

    Change icon color

--- components/MobileMenuButton.tsx
+++ components/MobileMenuButton.tsx
@@ -4,7 +4,7 @@ import { FaBars } from "react-icons/fa6";
 function MobileMenuButton({ onClick }) {
   return (
     <button className="absolute right-2 top-2 p-2 " onClick={onClick}>
-      <FaBars className="text-2xl" />;
+      <FaBars className="text-2xl text-[#464DAF]" />;
     </button>
   );
 }
