5815ef7b6fff50f8c24fa0a56a27d4ac26efbd4d
Norma Granados <paraisoocho@gmail.com>
2024-04-11 11:27:47 -0600

    Update portfolio section

--- components/PortfolioSection.tsx
+++ components/PortfolioSection.tsx
@@ -5,71 +5,48 @@ import data4 from "../public/data4.jpg";
 function Portfolio() {
   return (
     <>
-      <section id="portafolio" className="px-10 md:px-20 lg:px-40">
-        <div>
-          <h3 className="text-3xl text-[#464DAF] py-1">Portafolio</h3>
+      <section id="portafolio" className="pt-20 min-h-screen">
+        <div className="px-10 md:px-20 lg:px-40 ">
+          <h3 className="font-Montserrat font-medium text-3xl text-[#464DAF] py-1">
+            Portafolio
+          </h3>
           <div></div>
-          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+          <div className="flex flex-col gap-5 py-10 lg:flex-row lg:flex-wrap">
             <div className=" basis-1/3 flex-1 group-hover:opacity-50 cursor-pointer">
-              <a className="relative block group">
+              <div className="relative block group">
                 <Image
                   alt=""
-                  src={data3}
-                  layout="responsive"
-                  className="absolute inset-0 rounded-lg w-full h-full object-cover"
+                  src={data4}
+                  className="absolute inset-0 rounded-lg w-full h-80 object-cover"
                 />
                 <div
-                  className="relative transition-all transform 
-                                translate-y-8 opacity-0 
-                                group-hover:opacity-100 w-full h-full
+                  className="relative transition-all rounded-lg opacity-0 
+                                group-hover:opacity-80 w-full h-80 bg-[#464DAF]
                                 "
                 >
-                  <div className="p-2">
-                    <p className="text-xl text-black bg-gray-300">test</p>
+                  <p className="text-xl text-black ">test</p>
+                </div>
+              </div>
+            </div>
+            <div className=" basis-1/3 flex-1 group-hover:opacity-50 cursor-pointer">
+              <div className="relative block group">
+                <Image
+                  alt=""
+                  src={data4}
+                  className="absolute inset-0 rounded-lg w-full h-80 object-cover"
+                />
+                <div
+                  className="relative transition-all rounded-lg opacity-0 
+                                group-hover:opacity-50 w-full h-80 bg-[#464DAF]
+                                "
+                >
+                  <div className="absolute z-20 ">
+                    <p className=" text-center text-2xl group-hover:opacity-100 text-white/100 ">
+                      test
+                    </p>
                   </div>
                 </div>
-              </a>
-            </div>
-
-            <div className="basis-1/3 flex-1 cursor-pointer">
-              <Image
-                alt=""
-                src={data3}
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                className="rounded-lg object-cover"
-              />
-            </div>
-            <div className="basis-1/3 flex-1 cursor-pointer">
-              <Image
-                alt=""
-                src={data4}
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                className="rounded-lg object-cover"
-              />
-            </div>
-            <div className="basis-1/3 flex-1 cursor-pointer">
-              <Image
-                alt=""
-                src={data4}
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                className="rounded-lg object-cover"
-              />
-            </div>
-            <div className="basis-1/3 flex-1 cursor-pointer">
-              <Image
-                alt=""
-                src={data3}
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                className="rounded-lg object-cover"
-              />
+              </div>
             </div>
           </div>
         </div>
