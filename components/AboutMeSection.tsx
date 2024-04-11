5991d84d45d11ff557060c411b3fcd6ff63dc657
Norma Granados <paraisoocho@gmail.com>
2024-04-11 11:16:17 -0600

    Make about me section responsive

--- components/AboutMeSection.tsx
+++ components/AboutMeSection.tsx
@@ -4,16 +4,18 @@ import normada from "../public/normagc.png";
 function About() {
   return (
     <>
-      <section id="sobre" className="bg-[#ECE4F2]  min-h-screen">
-        <div className="px-10 md:px-20 lg:px-40">
+      <section id="sobre" className="pt-20 bg-[#F5F1F8]  min-h-screen">
+        <div className=" px-10 md:px-20 lg:px-40">
           <div>
-            <h3 className="text-3xl text-[#464DAF] py-1">Sobre mí</h3>
+            <h3 className="font-Montserrat font-medium text-3xl text-[#464DAF] py-1">
+              Sobre mí
+            </h3>
           </div>
-          <div className="flex p-10">
+          <div className="lg:flex p-10 font-Raleway">
             <div className="relative rounded-full w-40 h-40 mt-5 overflow-hidden md:h-60 md:w-60 ">
               <Image src={normada} alt="" layout="fill" objectFit="cover" />
             </div>
-            <div className="ml-40">
+            <div className="lg:ml-40">
               <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
                 ¡Hola, mi nombre es Patricia!
               </p>
