ae371643c3f4115f9aa9ff4b0faa51330fddcd9b
Norma Granados <paraisoocho@gmail.com>
2024-07-25 11:24:19 -0600

    update portfolio section

--- components/PortfolioSection.tsx
+++ components/PortfolioSection.tsx
@@ -3,10 +3,10 @@ import Tabs from "./Tabs";
 function Portfolio() {
   return (
     <>
-      <section id="portafolio" className="pt-20 min-h-screen">
+      <section id="portfolio" className="pt-10 min-h-screen">
         <div className="px-10 md:px-20 lg:px-40 ">
           <h3 className="font-Montserrat font-medium text-3xl text-[#464DAF] py-1">
-            Portafolio
+            Portfolio
           </h3>
           <div></div>
           <Tabs />
