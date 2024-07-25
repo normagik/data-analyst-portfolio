35e4f297c093b5f0f0642220b45259403965fd65
Norma Granados <paraisoocho@gmail.com>
2024-07-25 11:22:52 -0600

    Update hero section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,13 +1,13 @@
 function Hero() {
   return (
     <>
-      <div id="#inicio" className="text-center p-10">
+      <div id="#home" className="text-center p-10">
         <h2 className="font-Montserrat font-semibold text-6xl py-2 text-[#464DAF]">
           Patricia Granados
         </h2>
-        <h3 className="font-Raleway text-3xl py-2">Analista de Datos</h3>
+        <h3 className="font-Raleway text-3xl py-2">Data Analyst</h3>
         <p className="font-Raleway text-xl py-5 leading-8 text-gray-800">
-          lorem ipsum
+          Empowering decisions through data-driven analysis.
         </p>
       </div>
     </>
