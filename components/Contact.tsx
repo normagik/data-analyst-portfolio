a070b7ea8a954cb289fa68187bc943d2a0ef74a4
Norma Granados <paraisoocho@gmail.com>
2024-07-25 11:21:38 -0600

    update contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -3,10 +3,10 @@ import ContactForm from "./ContactForm";
 function Contact() {
   return (
     <>
-      <section id="contacto" className="pt-10 min-h-screen">
+      <section id="contact" className="pt-10 min-h-screen">
         <div className=" px-10 md:px-20 lg:px-40 ">
           <h3 className="font-Montserrat font-medium text-3xl text-[#464DAF]  py-1">
-            Contacto
+            Contact
           </h3>
           <div className="">
             <ContactForm />
--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -54,7 +54,7 @@ function ContactForm() {
   };
 
   return (
-    <div className="flex justify-center items-center">
+    <div className="pt-10 pb-20 flex justify-center items-center">
       <p></p>
       {formSuccess ? (
         <div>{formSuccessMessage}</div>
@@ -63,40 +63,53 @@ function ContactForm() {
           method="POST"
           action="https://www.formbackend.com/f/664decaabbf1c319"
           onSubmit={submitForm}
-          className="w-full max-w-lg bg-white shadow-xl border border-solid border-gray-200 rounded-lg px-8 pt-6 pb-8 mb-4"
+          className="w-full lg:w-3/5 lg:max-w-lg bg-white shadow-xl border border-solid border-gray-200 rounded-lg px-8 pt-6 pb-8 mb-4"
         >
-          <div className="flex flex-col py-5">
-            <label>Name</label>
+          <div className="flex flex-col py-5 px-2 ">
+            <label className="font-Montserrat font-medium text-lg text-[#464DAF] pb-2 items-center">
+              Name <span className="text-red-500 pl-1">*</span>
+            </label>
             <input
               type="text"
               name="name"
               onChange={handleInput}
               value={formData.name}
-              className="shadow appearance-none border rounded w-3/4"
+              className="font-Raleway capitalize shadow appearance-none  border-[.5px] border-[#464DAF] rounded w-full px-2 py-2"
             />
           </div>
 
-          <div className="flex flex-col py-5">
-            <label>Email</label>
+          <div className="flex flex-col py-3 px-2">
+            <label className="font-Montserrat font-medium text-lg text-[#464DAF] pb-2">
+              Email<span className="text-red-500 pl-1">*</span>
+            </label>
             <input
               type="text"
               name="email"
               onChange={handleInput}
               value={formData.email}
-              className="shadow appearance-none border rounded w-3/4"
+              className="font-Raleway shadow appearance-none border-[.5px] border-[#464DAF] rounded w-full px-2 py-2 "
             />
           </div>
 
-          <div className="flex flex-col py-5">
-            <label>Message</label>
+          <div className="flex flex-col py-3 px-2">
+            <label className="font-Montserrat font-medium text-lg text-[#464DAF] pb-2">
+              Message <span className="text-red-500 pl-1">*</span>
+            </label>
             <textarea
               name="message"
               onChange={handleInput}
               value={formData.message}
+              className="font-Raleway capitalize shadow appearance-none border-[.5px] border-[#464DAF] rounded w-full px-2 py-2"
             ></textarea>
           </div>
-
-          <button type="submit">Send message</button>
+          <div className="pt-4">
+            <button
+              type="submit"
+              className="w-full font-Montserrat font-medium border-solid border-2 border-[#464DAF] hover:bg-white hover:text-black px-6 py-2  rounded-xl ml-2  cursor-pointer bg-[#464DAF] text-white"
+            >
+              Send
+            </button>
+          </div>
         </form>
       )}
     </div>
