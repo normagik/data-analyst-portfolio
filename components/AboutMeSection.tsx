f6a2ef5d7b37914e2d5f809da1e258f95a610eb9
Norma Granados <paraisoocho@gmail.com>
2024-07-25 11:21:16 -0600

    update about me section

--- components/AboutMeSection.tsx
+++ components/AboutMeSection.tsx
@@ -4,11 +4,11 @@ import normada from "../public/normagc.png";
 function About() {
   return (
     <>
-      <section id="sobre" className="pt-10 bg-[#F5F1F8]  min-h-screen">
+      <section id="about" className="pt-10 bg-[#F5F1F8]  min-h-screen">
         <div className=" px-10 md:px-20 lg:px-40">
           <div>
             <h3 className="font-Montserrat font-medium text-3xl text-[#464DAF] py-1">
-              Sobre mí
+              About me
             </h3>
           </div>
           <div className="lg:flex p-10 font-Raleway">
@@ -17,13 +17,16 @@ function About() {
             </div>
             <div className="lg:ml-40">
               <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Hi, I'm Patricia Granados, an industrial engineer with a passion
+                for data analysis and front-end development. I hold an IBM
+                certification in data analysis and recently completed a job
+                simulation with Accenture. While new to professional data
+                analysis, my experience as a remote Front End Developer has
+                given me a strong foundation in programming and problem-solving.
+                My industrial engineering background and technical skills allow
+                me to leverage data to drive insights and inform decisions. I
+                enjoy reading and doing origami, which enhance my creativity and
+                continuous learning.
               </p>
             </div>
           </div>
