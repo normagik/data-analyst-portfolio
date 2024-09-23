76005d365999945bdd3ed17acdedf4d991e3eeac
Norma Granados <paraisoocho@gmail.com>
2024-09-23 14:47:55 -0600

    Portfolio update

--- components/DataAnalysisPortfolio.tsx
+++ components/DataAnalysisPortfolio.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Card from "./CardItem";
 import data4 from "../public/data4.jpg";
+import Link from "next/link";
 
 const projects = [
   {
     id: 1,
     src: "/data4.jpg",
+    link: "/projects/ab-testing",
     tools: "Python, Tableau",
     title: "A/B Testing | Python | Tableau",
     info: "Lorem ipsum",
@@ -13,13 +15,15 @@ const projects = [
   {
     id: 2,
     src: "/data4.jpg",
-    tools: "Python, Tableau",
-    title: "RFM Analysis | Tableau | Python",
+    link: "/projects/rfm_analysis",
+    tools: "Python",
+    title: "RFM Analysis | Python",
     info: "Lorem ipsum",
   },
   {
     id: 3,
     src: "/data4.jpg",
+    link: "/",
     tools: "Python, Tableau",
     title: " | Tableau | Python",
     info: "Lorem ipsum",
@@ -27,6 +31,7 @@ const projects = [
   {
     id: 4,
     src: "/data4.jpg",
+    link: "/",
     tools: "Python, Tableau",
     title: " | Tableau | Python",
     info: "Lorem ipsum",
@@ -37,7 +42,11 @@ function Data() {
     <>
       <div className="flex flex-col gap-5 py-10 lg:flex-row lg:flex-wrap">
         {projects.map((project: any) => {
-          return <Card project={project} key={project.id} />;
+          return (
+            <Link href={project.link}>
+              <Card project={project} key={project.id} />
+            </Link>
+          );
         })}
       </div>
     </>
